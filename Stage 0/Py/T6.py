dd06bb9bc6979d6c3cf97a180e4a0290741aa3cc
Aaroen <1126904957@qq.com>
2025-03-04 18:19:46 +0800

    6

--- Stage 0/Py/T6.py
+++ Stage 0/Py/T6.py
@@ -3,4 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        
+        j = 0
+        for i in range(len(nums)):
+            if nums[i] !=0:
+                if i != j:
+                    nums[j], nums[i] = nums[i], nums[j]
+                j += 1
