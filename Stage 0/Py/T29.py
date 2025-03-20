0f365cf33b92f262c51a661812139929362067ef
Aaroen <1126904957@qq.com>
2025-03-20 21:19:41 +0800

    29

--- Stage 0/Py/T29.py
+++ Stage 0/Py/T29.py
@@ -1,3 +1,31 @@
+# class Solution:
+#     def myPow(self, x: float, n: int) -> float:
+#         result = 1
+#         if n > 0:
+#             for i in range (n):
+#                 result = result * x
+#             return result
+#         elif n == 0:
+#             return 1
+#         else:
+#             n = -n
+#             for i in range (n):
+#                 result = result * x
+#             return 1/result
+            
+
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        
+        if n == 0:
+            return 1.0
+        if n < 0:
+            x = 1.0 / x
+            n = -n
+
+        result = 1.0
+        while n > 0:
+            if n % 2 == 1:
+                result *= x
+            x *= x
+            n //= 2
+        return result
